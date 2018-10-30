9b31c7845a437f2d9a0059b29e057099fc067b87
Yanga Zukelwa <yangazukelwa@gmail.com>
2018-10-30 16:55:42 +0200

    final commit of the project Eat Cupcake

--- EatCupcake.java
+++ EatCupcake.java
@@ -11,107 +11,107 @@ public class EatCupcake {
         System.out.println("Did anyone see you?");
 
         //declares a variable that reads in a line of keyboard input.
-        String myString = sc.next().toLowerCase();
+        String myString = sc.next().toUpperCase();
 
         switch (myString) {
 
-            case "yes":
-                System.out.println("Was it a boss/lover/parent?. ");
-                String firstInput = sc.next().toLowerCase();
+            case "YES.":
+                System.out.println("Was it a boss/lover/parent?");
+                String firstInput = sc.next().toUpperCase();
 
-                if (firstInput.equals("yes")) {
-                    System.out.println("Was it expensive?. ");
-                    String secInput = sc.next().toLowerCase();
+                if (firstInput.equals("YES.")) {
+                    System.out.println("Was it expensive?");
+                    String secInput = sc.next().toUpperCase();
 
-                    if (secInput.equals("yes")) {
-                        System.out.println("Can you cut off the part that touched the floor?. ");
-                        String insideInput = sc.next().toLowerCase();
+                    if (secInput.equals("YES.")) {
+                        System.out.println("Can you cut off the part that touched the floor?");
+                        String insideInput = sc.next().toUpperCase();
 
-                        if (insideInput.equals("yes")) {
-                            System.out.println("EAT IT");
+                        if (insideInput.equals("YES.")) {
+                            System.out.println("EAT IT.");
 
                         } else {
                             System.out.println("YOUR CALL");
 
                         }
                     } else {
-                        System.out.println("Is it chocolate? ");
-                        String chocInput = sc.next().toLowerCase();
-                        if (chocInput.equals("yes")) {
-                            System.out.println("EAT IT. ");
+                        System.out.println("Is it chocolate?");
+                        String chocInput = sc.next().toUpperCase();
+                        if (chocInput.equals("YES.")) {
+                            System.out.println("EAT IT.");
                         } else {
                             System.out.println("DON'T EAT IT");
                         }
                     }
 
                 } else {
-                    System.out.println("EAT IT. ");
+                    System.out.println("EAT IT.");
                 }
 
                 break;
 
 
-            case "no":
-                System.out.println("Was it sticky?. ");
-                String stickyAns = sc.next().toLowerCase();
+            case "NO.":
+                System.out.println("Was it sticky?");
+                String stickyAns = sc.next().toUpperCase();
 
-                if (stickyAns.equals("yes")) {
-                    System.out.println("Is it a raw steak?. ");
-                    String steakAns = sc.next().toLowerCase();
+                if (stickyAns.equals("YES.")) {
+                    System.out.println("Is it a raw steak?");
+                    String steakAns = sc.next().toUpperCase();
 
-                    if (steakAns.equals("yes")) {
+                    if (steakAns.equals("YES.")) {
                         System.out.println("Are you a puma?.");
-                        String pumaAns = sc.next().toLowerCase();
-                        if (pumaAns.equals("yes")) {
-                            System.out.println("EAT IT");
+                        String pumaAns = sc.next().toUpperCase();
+                        if (pumaAns.equals("YES.")) {
+                            System.out.println("EAT IT.");
                         } else {
                             System.out.println("DON'T EAT IT");
                         }
                     } else {
-                        System.out.println("Did the cat lick it?. ");
-                        String lickAns = sc.next().toLowerCase();
+                        System.out.println("Did the cat lick it?");
+                        String lickAns = sc.next().toUpperCase();
 
-                        if (lickAns.equals("yes")) {
-                            System.out.println("Is your cat healthy?.");
-                            String healthyAns = sc.next().toLowerCase();
+                        if (lickAns.equals("YES.")) {
+                            System.out.println("Is your cat healthy?");
+                            String healthyAns = sc.next().toUpperCase();
 
-                            if (healthyAns.equals("yes")) {
-                                System.out.println("EAT IT");
+                            if (healthyAns.equals("YES.")) {
+                                System.out.println("EAT IT.");
                             } else {
                                 System.out.println("YOUR CALL");
                             }
                         } else {
-                            System.out.println("EAT IT");
+                            System.out.println("EAT IT.");
                         }
                     }
                 } else {
-                    System.out.println("Is it an Emausaurus?. ");
-                    String emaAns = sc.next().toLowerCase();
+                    System.out.println("Is it an Emausaurus?");
+                    String emaAns = sc.next().toUpperCase();
 
-                    if (emaAns.equals("yes")) {
-                        System.out.println("Are you a Megalosaurus?. ");
-                        String megaAns = sc.next().toLowerCase();
+                    if (emaAns.equals("YES.")) {
+                        System.out.println("Are you a Megalosaurus?");
+                        String megaAns = sc.next().toUpperCase();
 
-                        if (megaAns.equals("yes")) {
-                            System.out.println("EAT IT");
+                        if (megaAns.equals("YES.")) {
+                            System.out.println("EAT IT.");
                         } else {
                             System.out.println("DON'T EAT IT");
                         }
                     } else {
-                        System.out.println("Did the cat lick it");
-                        String catAns = sc.next().toLowerCase();
+                        System.out.println("Did the cat lick it?");
+                        String catAns = sc.next().toUpperCase();
 
-                        if (catAns.equals("yes")) {
-                            System.out.println("Is your cat healthy?. ");
-                            String catHealthyAns = sc.next().toLowerCase();
+                        if (catAns.equals("YES.")) {
+                            System.out.println("Is your cat healthy?");
+                            String catHealthyAns = sc.next().toUpperCase();
 
-                            if (catHealthyAns.equals("yes")) {
-                                System.out.println("EAT IT");
+                            if (catHealthyAns.equals("YES.")) {
+                                System.out.println("EAT IT.");
                             } else {
                                 System.out.println("YOUR CALL");
                             }
                         } else {
-                            System.out.println("EAT IT. ");
+                            System.out.println("EAT IT.");
                         }
                     }
                 }
@@ -120,7 +120,6 @@ public class EatCupcake {
 
 
             default:
-                System.out.println("Invalid input, either 'yes' or 'no'. ");
                 break;
         }
 
