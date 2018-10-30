b7a74d0727947247193da276ff537d4e63411f15
Yanga Zukelwa <yangazukelwa@gmail.com>
2018-10-30 15:20:29 +0200

    modified eat cupcake file with second logical flow to take the no input

--- EatCupcake.java
+++ EatCupcake.java
@@ -53,6 +53,68 @@ public class EatCupcake {
 
             case "no":
                 System.out.println("Was it sticky?. ");
+                String stickyAns = sc.next().toLowerCase();
+
+                if (stickyAns.equals("yes")) {
+                    System.out.println("Is it a raw steak?. ");
+                    String steakAns = sc.next().toLowerCase();
+
+                    if (steakAns.equals("yes")) {
+                        System.out.println("Are you a puma?.");
+                        String pumaAns = sc.next().toLowerCase();
+                        if (pumaAns.equals("yes")) {
+                            System.out.println("EAT IT");
+                        } else {
+                            System.out.println("DON'T EAT IT");
+                        }
+                    } else {
+                        System.out.println("Did the cat lick it?. ");
+                        String lickAns = sc.next().toLowerCase();
+
+                        if (lickAns.equals("yes")) {
+                            System.out.println("Is your cat healthy?.");
+                            String healthyAns = sc.next().toLowerCase();
+
+                            if (healthyAns.equals("yes")) {
+                                System.out.println("EAT IT");
+                            } else {
+                                System.out.println("YOUR CALL");
+                            }
+                        } else {
+                            System.out.println("EAT IT");
+                        }
+                    }
+                } else {
+                    System.out.println("Is it an Emausaurus?. ");
+                    String emaAns = sc.next().toLowerCase();
+
+                    if (emaAns.equals("yes")) {
+                        System.out.println("Are you a Megalosaurus?. ");
+                        String megaAns = sc.next().toLowerCase();
+
+                        if (megaAns.equals("yes")) {
+                            System.out.println("EAT IT");
+                        } else {
+                            System.out.println("DON'T EAT IT");
+                        }
+                    } else {
+                        System.out.println("Did the cat lick it");
+                        String catAns = sc.next().toLowerCase();
+
+                        if (catAns.equals("yes")) {
+                            System.out.println("Is your cat healthy?. ");
+                            String catHealthyAns = sc.next().toLowerCase();
+
+                            if (catHealthyAns.equals("yes")) {
+                                System.out.println("EAT IT");
+                            } else {
+                                System.out.println("YOUR CALL");
+                            }
+                        } else {
+                            System.out.println("EAT IT. ");
+                        }
+                    }
+                }
 
                 break;
 
@@ -61,7 +123,6 @@ public class EatCupcake {
                 System.out.println("Invalid input, either 'yes' or 'no'. ");
                 break;
         }
-
 
     }
 }
